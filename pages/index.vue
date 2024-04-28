ff68e7b9c454f6ed6fcac3ee287b980bdb804f43
Krishna Sameer Achanta <achanta3215@gmail.com>
2024-04-28 10:19:31 +0000

    Display search results

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,23 @@
 <template>
-    hello
+    <div>
+        <h1>Search Results</h1>
+        <ul>
+            <li v-for="movie in results" :key="movie.id">
+                {{ movie.title }}
+                <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" alt="Movie poster">
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script setup>
-    const { tmdbAccessToken } = useRuntimeConfig()
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${tmdbAccessToken}`
-    };
+const { tmdbAccessToken } = useRuntimeConfig()
+const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${tmdbAccessToken}`
+};
 
-    const { data } = await getApi(useNuxtApp(), 'https://api.themoviedb.org/3/search/movie', { headers, query: { query: 'The+Matrix' } });
+const { data } = await getApi(useNuxtApp(), 'https://api.themoviedb.org/3/search/movie', { headers, query: { query: 'The+Matrix' } });
+const results = ref(data.value.results);
+
 </script>
